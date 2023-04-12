f6805cde2033dc915f22bb3cb500169c967f034e
Attit Pudasaini <attitpudasaini@Attits-MacBook-Air.local>
2023-04-12 11:56:31 -0400

    update- user login and registration complete methods added for generating jwt and hashed the user password

--- src/models/user.js
+++ src/models/user.js
@@ -1,46 +1,97 @@
-const mongoose = require("mongoose")
-const validator = require("validator")
+const bcrypt = require("bcryptjs/dist/bcrypt");
+const mongoose = require("mongoose");
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
-    name:{
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Please enter a valid age!");
+      }
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Invalid Email Address!");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    validate(value) {
+      if (value.toLowerCase() === "password") {
+        throw new Error("Password cannot contain 'password'");
+      }
+      if (value.length < 6) {
+        throw new Error("Password length must be greater than 6");
+      }
+    },
+  },
+  tokens: [
+    {
+      token: {
         type: String,
         required: true,
-        trim: true
+      },
     },
-    age:{
-        type: Number,
-        default: 0,
-        validate(value){
-            if(value < 0){
-                throw new Error("Please enter a valid age!")
-            }
-        }
-    },
-    email:{
-        type: String,
-        required: true,
-        trim: true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error("Invalid Email Address!")
-            }
-        }
-    },
-    password:{
-        type: String,
-        required: true,
-        trim: true,
-        validate(value){
-            if(value.toLowerCase() === 'password'){
-                throw new Error("Password cannot contain 'password'")
-            }
-            if(value.length < 6){
-                throw new Error("Password length must be greater than 6")
-            }
-        }
-    }
+  ],
+});
 
-})
+//hash the user password
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+});
 
-const User = mongoose.model("User", userSchema)
-module.exports = User
+//generate auth token
+//methods are accessible on the instances
+userSchema.methods.generateAuthToken = async function () {
+  try {
+    //not necessary but makes life easier
+    const user = this;
+    //create a token
+    const token = jwt.sign({ _id: user._id.toString() }, "thisismynewcourse");
+    //adding the token in user instance
+    user.tokens = user.tokens.concat({ token });
+    await user.save();
+    return token;
+  } catch (e) {
+    console.log("❌Error generating auth token!", e);
+    throw new Error({ e });
+  }
+};
+
+//statics function are accessible to model
+userSchema.statics.findByCredentials = async (email, password) => {
+  //finding user by the email address
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error("Unable to login!");
+  }
+  //comparing user entered password with the stored password
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error("Unable to login!");
+  }
+  return user;
+};
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,15 +1,32 @@
 const express = require("express");
+const User = require("../models/user");
 
 const router = new express.Router();
 
 //create user
 router.post("/user", async (req, res) => {
-  res.send("🚧 ENDPOINT UNDER CONSTRUCTION");
+  try {
+    //creating an instance of the user
+    const user = new User(req.body);
+    //generating token for the user instance and saving it in the database
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
+  } catch (error) {
+    res.status(401).send({ error });
+  }
 });
 
 //login user
 router.post("/user/login", async (req, res) => {
-  res.send("🚧 ENDPOINT UNDER CONSTRUCTION");
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    res.send(user);
+  } catch (error) {
+    res.status(401).send({ error });
+  }
 });
 
 //logout from one device
@@ -19,8 +36,8 @@ router.post("/user/logout", async (req, res) => {
 
 //logout from all device
 router.post("/user/logoutAll", async (req, res) => {
-    res.send("🚧 ENDPOINT UNDER CONSTRUCTION");
-  });
+  res.send("🚧 ENDPOINT UNDER CONSTRUCTION");
+});
 
 //get  user
 router.get("/user/me", async (req, res) => {
