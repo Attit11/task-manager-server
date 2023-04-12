caa139d66b0bab03977f334f1a8846d43ca24c2c
Attit Pudasaini <attitpudasaini@Attits-MacBook-Air.local>
2023-04-12 12:52:54 -0400

    user endpoints complete but need to refactor it in future

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const User = require("../models/user");
+const auth = require("../middleware/auth");
 
 const router = new express.Router();
 
@@ -19,39 +20,79 @@ router.post("/user", async (req, res) => {
 //login user
 router.post("/user/login", async (req, res) => {
   try {
+    //using self defined model function to find the user authenticate it.
     const user = await User.findByCredentials(
       req.body.email,
       req.body.password
     );
-    res.send(user);
+    //generating a token to login successfully
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
   } catch (error) {
     res.status(401).send({ error });
   }
 });
 
 //logout from one device
-router.post("/user/logout", async (req, res) => {
-  res.send("🚧 ENDPOINT UNDER CONSTRUCTION");
+router.post("/user/logout", auth, async (req, res) => {
+  try {
+    //removing the current token form the database
+    req.user.tokens = req.user.tokens.filter(
+      (token) => token.token !== req.token
+    );
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(401).send(error);
+  }
 });
 
 //logout from all device
-router.post("/user/logoutAll", async (req, res) => {
-  res.send("🚧 ENDPOINT UNDER CONSTRUCTION");
+router.post("/user/logoutAll", auth, async (req, res) => {
+  try {
+    //removes all the token from the database
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(401).send(error);
+  }
 });
 
 //get  user
-router.get("/user/me", async (req, res) => {
-  res.send("🚧 ENDPOINT UNDER CONSTRUCTION");
+router.get("/user/me", auth, async (req, res) => {
+  try {
+    res.send(req.user);
+  } catch (error) {
+    res.status(401).send(error);
+  }
 });
 
 //delete user
-router.delete("/user", async (req, res) => {
-  res.send("🚧 ENDPOINT UNDER CONSTRUCTION");
+router.delete("/user", auth, async (req, res) => {
+  try {
+    await User.findOneAndDelete({ _id: req.user._id });
+    res.send({ user: "User Deleted!" });
+  } catch (e) {
+    res.send(401).send(e);
+  }
 });
 
 //update user
-router.patch("/user/:id", async (req, res) => {
-  res.send("🚧 ENDPOINT UNDER CONSTRUCTION");
+router.patch("/user/me",auth, async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdated = ["name", "age", "email", "password"];
+  const isValid = updates.every((update) => allowedUpdated.includes(update));
+  if (!isValid) {
+    res.status(401).send({ error: "Invalid Updates!" });
+  }
+  try {
+    updates.forEach((update) => (req.user[update] = req.body[update]));
+    await req.user.save();
+    res.send(req.user)
+  } catch (error) {
+    res.status(401).send(error);
+  }
 });
 
 module.exports = router;
