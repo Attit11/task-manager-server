f55bb384636ffcf7cb142a8c937bfa2bf83cf68d
Attit11 <sharmaattit09@gmail.com>
2023-04-12 16:58:07 -0400

    database connection string fixed

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose")
 
-const uri = 'mongodb://127.0.0.1:27017/task-man'
+const uri = "mongodb+srv://attit11:Thinkpad11@cluster0.ehix2s4.mongodb.net/task-man"
 
 mongoose.connect(uri).then(
     () => { 
         console.log("🚀 Connected to Database!")
      },
     (err) => { 
-        console.log(`❌ ${{err}}`)
+        console.log(`❌ ${err}`)
      }
   )
