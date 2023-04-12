1ed4613a222cea939db52092d76aad2b86ef7a06
Attit11 <sharmaattit09@gmail.com>
2023-04-12 17:04:12 -0400

    fixing cors error

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express")
+var cors = require('cors')
 require("./database/mongoose")
 const userRouter = require("./routers/user")
 const taskRouter = require("./routers/task")
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
